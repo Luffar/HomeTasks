4123c2a77de15d0e636c95b9a1ca12f053b55674
drunbultt <mathotopa512@gmail.com>
2022-11-20 22:20:39 +0200

    -Task9

--- src/test/java/Task9/GetTitle.java
+++ src/test/java/Task9/GetTitle.java
@@ -1,16 +1,18 @@
 package Task9;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GetTitle {
+
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
         title("https://ithillel.ua/");
     }
 
-    public static void title(String url){
-        System.setProperty("webdriver.chrome.driver", "C:\\webdriwers\\chromedriver.exe");
+    public static void title(String url) {
         WebDriver driver = new ChromeDriver();
         driver.get(url);
         System.out.println(driver.getTitle());
@@ -18,3 +20,4 @@ public class GetTitle {
     }
 
 }
+
