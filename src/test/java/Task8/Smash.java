337d9d4886877db81618988418fdb7e977a54b81
drunbultt <mathotopa512@gmail.com>
2022-11-20 21:06:08 +0200

    -Task8

--- src/test/java/Task8/Smash.java
+++ src/test/java/Task8/Smash.java
@@ -4,19 +4,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Smash {
-    public static void firstUpperCase(String word) {
-        String[] words = word.split(", ");
-        Set<String> items = new HashSet<>(Arrays.asList(words));
-        for (String s : items) {
-            System.out.println(s);
-        }
-    }
-    public static String ss (String str){
-        return str.substring(0, 1).toUpperCase() + str.substring(1);
-    }
-
     public static void main(String[] args) {
         String programmingLanguagesName = "java, python, java, c#, ruby, python, c++, ruby";
-        firstUpperCase(ss(programmingLanguagesName));
+        firstUpperCase(programmingLanguagesName);
+    }
+
+    private static void firstUpperCase(String programmingLanguagesName) {
+        String[] words = programmingLanguagesName.split(", ");
+        Set<String> items = new HashSet<>(Arrays.asList(words));
+        for (String s:items){
+            System.out.println(s.substring(0, 1).toUpperCase() + s.substring(1));
+        }
     }
 }
